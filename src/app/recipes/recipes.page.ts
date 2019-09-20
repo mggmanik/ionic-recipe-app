c413171ed8a254866ed101afcc2362f7963c6541
Manik <mggmanik@gmail.com>
2019-09-20 12:49:13 +0530

    fixed bug - ionViewWillEnter

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -15,10 +15,9 @@ export class RecipesPage implements OnInit {
     }
 
     ngOnInit() {
-        this.recipes = this.recipesService.getAllRecipes();
     }
 
-    ionViewWillEnter(){
+    ionViewWillEnter() {
         this.recipes = this.recipesService.getAllRecipes();
     }
 
